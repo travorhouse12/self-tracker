4589e578c68397f2ba381ec0538211d5cf2620b4
Macbook <macbook@MacBook-Pro-6.local>
2023-03-13 21:42:09 -0700

    Create an area chart using the altair library

--- project_files/pages/2_💤_Sleep.py
+++ project_files/pages/2_💤_Sleep.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
+import altair as alt
 from sidebar import add_sidebar
 
 # Read in the CSV file
-df = pd.read_csv('https://raw.githubusercontent.com/travorhouse12/self-tracker/main/project_files/total_sleep.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/travorhouse12/self-tracker/main/project_files/daily_sleep.csv')
 
 # Set page layout
 st.set_page_config(
@@ -13,9 +14,24 @@ st.set_page_config(
 
 add_sidebar()
 
-# Add the profile image
-image_url = "https://datadayz.com/wp-content/uploads/2023/03/Group-310-1.png"
-st.image(image_url, width=75)
-
 # Set the title of the app
-st.title("Travor's health tracker! 👋")
+st.title("Activity Hub")
+
+# Set empty space
+space = st.empty()
+space.header("")
+
+
+seven_days_back = pd.to_datetime('today').date() - pd.Timedelta(days=7)
+
+df = df.loc[df['day'] >= str(seven_days_back)]
+
+altairers = alt.Chart(df).mark_area().encode(
+    x="day",
+    y="total_sleep",
+    color="source:N"
+)
+
+st.altair_chart(altairers, use_container_width=True)
+
+# st.area_chart(df, x='day', y='total_sleep')
