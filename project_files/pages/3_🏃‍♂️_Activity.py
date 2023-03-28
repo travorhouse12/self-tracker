67a914652c8f9021c1dee8a4bdb16331f3bdd675
Macbook <macbook@MacBook-Pro-6.local>
2023-03-27 21:06:26 -0700

    Made changes to activity page

--- project_files/pages/3_🏃‍♂️_Activity.py
+++ project_files/pages/3_🏃‍♂️_Activity.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
+import altair as alt
+from datetime import datetime, timedelta
 from sidebar import add_sidebar
 
 # Read in the CSV file
-df = pd.read_csv('https://raw.githubusercontent.com/travorhouse12/self-tracker/main/project_files/total_sleep.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/travorhouse12/self-tracker/main/project_files/activity.csv')
 
 # Set page layout
 st.set_page_config(
@@ -14,12 +16,21 @@ st.set_page_config(
 add_sidebar()
 
 # Set the title of the app
-st.title("Activity Hub")
+st.title("🏃‍♂️ Activity Hub")
 
 # Set empty space
 space = st.empty()
 space.header("")
 
+
+# Filter to only include the date range needed for the chart
+df['activity_time'] = (df['activity_time'] / 3600).round(2)
+
 # Create a bar chart to display the activity data
-df['date'] = df['date'].astype('category')
-st.bar_chart(df, x='date', y='Total Sleep Duration')
+chart = alt.Chart(df).mark_bar().encode(
+    x='day',
+    y=alt.Y('activity_time', title='Total Hours'),
+    color='activity_type:N'
+)
+
+st.altair_chart(chart)
